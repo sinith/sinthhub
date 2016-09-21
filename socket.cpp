a47689e6fd61e61492b551a56e60304bee6a6cc5
sinith <sinith@qburst.com>
2016-09-21 09:45:15 +0530

    socket program

---
+++ socket.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+}
+
+char buffer[20];
+
+int a,o;
+char a[10];
+  fun(int a,int b)
+{
+ for(i=0;i>
